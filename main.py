ab79413b4d68128bdedb27be52b5533c58dbd233
lia-avetisyan <avetisyanlia0@gmail.com>
2026-05-28 23:10:31 +0400

    Did the panorama with Cur - Mur version

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class PanoramaStitcher:
         return panorama
 
 def main():
-    image_folder = "images"
+    image_folder = "images5"
     valid_formats = ('.jpg', '.jpeg', '.png', '.bmp', '.svg')
 
     if not os.path.exists(image_folder):
